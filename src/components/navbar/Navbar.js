5b36c1e976cddf4c5e5e9030e166f4e87ad0c7b6
n1ckk007 <antonellos101@gmail.com>
2020-11-03 00:02:54 +1100

    added icon and menu link to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Nav } from "./NavbarStyles";
+import { Bars, Nav, NavIcon, NavLink } from "./NavbarStyles";
 
 const Navbar = () => {
   return (
     <>
-      <Nav>Nav</Nav>
+      <Nav>
+        <NavLink to="/">Pizza</NavLink>
+        <NavIcon>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
     </>
   );
 };
