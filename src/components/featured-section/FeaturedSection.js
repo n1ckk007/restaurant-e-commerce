c6186d6ea878366317cb3e2e496bce1afd0a9bed
n1ckk007 <antonellos101@gmail.com>
2020-11-08 00:16:29 +1100

    styled feature component

--- src/components/featured-section/FeaturedSection.js
+++ src/components/featured-section/FeaturedSection.js
@@ -5,8 +5,8 @@ const FeaturedSection = () => {
   return (
     <FeatureContainer>
       <h1>Pizza of the Day</h1>
-      <p>Truffle alfredo sauce</p>
-      <FeatureButton></FeatureButton>
+      <p>Truffle alfredo sauce topped with goodness</p>
+      <FeatureButton>Order Now</FeatureButton>
     </FeatureContainer>
   );
 };
