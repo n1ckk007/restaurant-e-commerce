f8253de7e629ee2a29a65f21e9a841773ece784f
n1ckk007 <antonellos101@gmail.com>
2020-11-08 00:43:35 +1100

    added and styled footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Products from "./components/products/Products";
 import { GlobalStyle } from "./globalStyles";
 import { productData, productDataTwo } from "./components/products/productData";
 import FeaturedSection from "./components/featured-section/FeaturedSection";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Products heading="Choose your favourite" data={productData} />
       <FeaturedSection />
       <Products heading="Sweet treats for you" data={productDataTwo} />
+      <Footer />
     </>
   );
 }
--- src/components/featured-section/FeaturedSectionStyles.js
+++ src/components/featured-section/FeaturedSectionStyles.js
@@ -4,8 +4,8 @@ import FeaturePic from "../../images/featured3.jpg";
 export const FeatureContainer = styled.div`
   background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)),
     url(${FeaturePic});
-  height: 500px;
-  /* max-height: 500px; */
+  height: 100vh;
+  max-height: 500px;
   background-position: center;
   background-size: cover;
   display: flex;
@@ -13,8 +13,6 @@ export const FeatureContainer = styled.div`
   justify-content: center;
   align-items: center;
   color: #fff;
-  /* text-align: center; */
-  /* padding: 0 1rem; */
 
   h1 {
     font-size: clamp(3rem, 5vw, 5rem);
